3c31ef302a0dc53b1d5c56e2699b30742b33609f
Aainakat <aainakatiyar11@gmail.com>
2026-01-31 16:36:54 +0530

    Add basic variables and list example in 01_basics.py

---
+++ 01_basics.py
@@ -0,0 +1,13 @@
+# Variables and Data Types
+
+name = "Aaina"
+age = 20
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+
+# List example
+skills = ["Python", "AI/ML", "Design"]
+print("Skills:", skills)
